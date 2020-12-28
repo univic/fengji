8f86e18634c04c870af6c4f9fff8ea473a2ce92f
univic <xyzhgwf@hotmail.com>
2020-12-28 23:53:56 +0800

    minor change

--- fengji-frontend/src/App.vue
+++ fengji-frontend/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <ItemList></ItemList>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ItemList from "./components/ItemList.vue";
 
 export default {
   name: 'App',
   components: {
+    ItemList,
     HelloWorld
   }
 }
---
+++ fengji-frontend/src/components/ItemList.vue
@@ -0,0 +1,49 @@
+<template>
+<el-row>
+  <el-col>
+    <span
+      class="el-icon-plus"
+      v-if="!newItemFocused"
+    ></span>
+    <el-checkbox
+      v-else
+    ></el-checkbox>
+  </el-col>
+  <el-col>
+    <el-input
+        v-on:click="newItemFocused=!newItemFocused"
+    >
+    </el-input>
+  </el-col>
+
+</el-row>
+
+</template>
+
+<script>
+export default {
+  name: "ItemList",
+  data () {
+    return {
+      newItemFocused: true,
+
+      exampleData: [{
+          name: "A"
+        },
+        {
+          name: "B"
+        }
+      ]
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
