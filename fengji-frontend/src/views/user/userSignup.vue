19cfdd991689a88fccae32055068fa0af00c7be6
univic <xyzhgwf@hotmail.com>
2021-01-18 23:48:09 +0800

    use el-form

--- fengji-frontend/src/views/user/userSignup.vue
+++ fengji-frontend/src/views/user/userSignup.vue
@@ -10,41 +10,69 @@
       <el-card
           shadow="always"
       >
-        <el-input
-            prefix-icon="el-icon-user"
-            v-model="inputUsername"
-            placeholder="请输入用户名"
 
-        ></el-input>
-        <el-input
-            v-model="inputPassword"
-            prefix-icon="el-icon-key"
-            placeholder="请输入密码"
-            show-password
-        ></el-input>
-        <el-input
-            v-model="inputRetypePassword"
-            prefix-icon="el-icon-key"
-            placeholder="请重复一次密码"
-            show-password
-        ></el-input>
-        <el-input
-            prefix-icon="el-icon-message"
-            v-model="inputEmail"
-            placeholder="请输入联系邮箱"
-        ></el-input>
-        <el-checkbox
-          v-model="obeyAgreement"
-          style="display: block"
-          class="obey-agreement"
-        >同意使用协议</el-checkbox>
-        <div class="h-captcha" data-sitekey="f1e8fd67-87db-4474-b654-ac89b61d9fe3"></div>
+        <el-form
+          :model="ruleForm"
+          status-icon
+          :rules="rules"
+          ref="ruleForm"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input
+              prefix-icon="el-icon-user"
+              v-model="ruleForm.username"
+              placeholder="请输入用户名"
 
-        <el-button
-            type="primary"
-            class="button"
-        >提交
-        </el-button>
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input
+              v-model="ruleForm.password"
+              prefix-icon="el-icon-key"
+              placeholder="请输入密码"
+              show-password
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码">
+            <el-input
+              v-model="ruleForm.confirmPassword"
+              prefix-icon="el-icon-key"
+              placeholder="请重复一次密码"
+              show-password
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              prefix-icon="el-icon-message"
+              v-model="ruleForm.email"
+              placeholder="请输入联系邮箱"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-checkbox
+              v-model="ruleForm.obeyAgreement"
+              style="display: block"
+              class="obey-agreement"
+            >同意使用协议</el-checkbox>
+          </el-form-item>
+          <div
+            class="h-captcha"
+            data-sitekey="f1e8fd67-87db-4474-b654-ac89b61d9fe3"
+            data-theme="dark"
+          ></div>
+          <el-form-item>
+            <el-button
+              type="primary"
+              class="button"
+            >提交
+            </el-button>
+          </el-form-item>
+
+        </el-form>
+
+
+
+
       </el-card>
     </div>
 
@@ -60,11 +88,29 @@ export default {
   name: "userSignup",
   data() {
     return {
-      inputUsername: null,
-      inputPassword: null,
-      inputRetypePassword: null,
-      inputEmail: null,
-      obeyAgreement: null,
+      ruleForm: {
+        username: null,
+        password: null,
+        confirmPassword: null,
+        email: null,
+        obeyAgreement: null,
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min:3, max: 5, message: '请输入用户名', trigger: 'blur' },
+        ],
+        password: [
+          { required:true, message: '请输入密码', trigger: 'blur' },
+          { min:6, max: 32, message: '密码长度应为6~32位', trigger: 'blur' },
+        ],
+        confirmPassword: [
+          { required:true, message: '请再输入一次密码', trigger: 'blur' },
+        ],
+        email: [
+          { required:true, message: '邮件地址', trigger: 'blur' },
+        ]
+      }
     }
   },
   mounted: function (){
@@ -78,6 +124,12 @@ export default {
       captchaScript.async
       captchaScript.defer
       document.body.appendChild(captchaScript)
+    },
+    submitForm() {
+
+    },
+    resetForm() {
+
     }
   }
 }
