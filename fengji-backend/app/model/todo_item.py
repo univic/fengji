2a3cbf73fe5107f603286095d928d116e9054af1
univic <xyzhgwf@hotmail.com>
2021-08-03 00:48:03 +0800

    updated model definition

---
+++ fengji-backend/app/model/todo_item.py
@@ -0,0 +1,20 @@
+import datetime
+from mongoengine import StringField, ReferenceField, DateTimeField, EmbeddedDocumentListField, FileField, ListField
+from app.model.tag_template import ItemTag
+from app.model.report_group import ReportGroup
+from app.model.user_model import User
+from app.lib.database import db
+
+
+class TodoItem(db.Document):
+    title = StringField(required=True)
+    tag_list = EmbeddedDocumentListField(ItemTag)
+    create_time = DateTimeField(default=datetime.datetime.now())
+    creator = ReferenceField(User, required=True)
+    owner = ReferenceField(User)
+    report_to_group = ListField(ReferenceField(ReportGroup, required=True))
+    start_time = DateTimeField()
+    planned_finish_time = DateTimeField()
+    actual_finish_time = DateTimeField()
+    description = StringField()
+    attachment = FileField()
